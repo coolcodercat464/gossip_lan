3d712a161681b79855e2ff6801b5a16fb1fc8500
coolcodercat464 <electrotriple@gmail.com>
2026-08-27 20:52:56 +1000

    Create pdf_tools.py

---
+++ src/pdf_tools.py
@@ -0,0 +1,30 @@
+import os
+
+# get data of pdf with all metadata removed
+def get_pdf_data_clean(file_path: str) -> tuple[str, str]:
+    # get out of the /src working directory
+    file_path = '../' + file_path
+
+    # check path exists
+    if os.path.exists(file_path):
+        # open pdf as a binary file
+        with open(file_path, 'rb') as file:
+            reader = PyPDF2.PdfReader(file)
+            writer = PyPDF2.PdfWriter()
+            
+            # copy pages without metadata
+            for page in reader.pages:
+                writer.add_page(page)
+                
+            # write to a bytes buffer
+            buffer = BytesIO()
+            writer.write(buffer)
+            buffer.seek(0)
+
+        # get bytes and their hash
+        data = buffer.read()
+        hashed = hashlib.sha256(data).hexdigest()
+        return data, hashed
+    else:
+        # file doesn't exist
+        return '', ''
